b320757f034b0d88bc0559225d4d248b4f5c7354
sifes <lonfark@gmail.com>
2024-01-14 15:36:05 +0200

    invert: fix logic

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,9 +1,10 @@
 // Reverse an array, you can't use .reverse()
 'use strict'
 const invert = (array) => {
-	const res = Object.keys(array)
-	for (let index = 0; index < array.length; index++) {
-		res[index] = array.pop()
+	const res = []
+	const length = array.length
+	for (let index = 0; index < length; index++) {
+		res[index] = array[length - index - 1]
 	}
 	return res
 }
